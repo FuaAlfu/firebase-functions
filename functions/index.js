d0ce2ca0982158901db50618123eb5f54714a181
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-02-19 14:39:37 +0300

    add

--- functions/index.js
+++ functions/index.js
@@ -12,20 +12,22 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
  
-const db = admin.firestore();
-exports.postComment = functions.https.onCall((data, context) => {
-    return db.collection('publicProfiles').where('userId', '==', context.auth.uid)
-        .limit(1)
-        .get()
-        .then( snapshot => {
-            return db.collection('comments').add({
-                text: data.text,
-                username: snapshot.docs[0].ref,
-                dataCreated: new Date(),
-                note: db.collection('notes').doc(data.noteId)
-            })
-        })
-})
+exports.postComment = functions.https.onCall(async(data, context) => {
+    const db = admin.firestore();
+    const snapshot = await db
+    .collection('publicProfiles')
+    .where('userId', '==', context.auth.uid)
+    .limit(1)
+    .get();
+       
+    await db.collection('comments').add({
+      text: data.text,
+      username: snapshot.docs[0].id,
+      dataCreated: new Date(),
+      note: db.collection('notes').doc(data.noteId)
+ });
+});
+
 
 
 //firebase deploy --only functions
